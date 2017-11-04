2dddd459d203cf34d76f16c542f1372edfbac43c
Mounika1494 <edulamounika08@gmail.com>
2017-11-04 09:19:40 +0000

    fixing git reppo

---
+++ src/sequencer.c
@@ -0,0 +1,123 @@
+#include "system.h"
+#include "sequencer.h"
+
+static int i = 0;
+
+void int_handler(){
+
+
+  mq_close(temp_mq);
+  mq_close(light_mq);
+  mq_close(proc_mq);
+  mq_close(log_mq);
+  mq_unlink(SNDRCV_MQ3);
+  mq_unlink(SNDRCV_MQ4);
+  mq_unlink(SNDRCV_MQ5);
+  mq_unlink(SNDRCV_MQ6);
+  printf("\n caught crl c \n");
+  exit(0);
+
+}
+
+void sighler (union sigval val){
+  //printf("Handler entered with value :%d for %d times\n", val.sival_int, ++i);
+  static int count;
+  int nbytes;
+  ++count;
+  char *proc_msg = "push_to_proc";
+  char *query_msg = "push_to_query";
+  message_t sensor_data ;
+
+
+  switch(count){
+
+    case 1:
+      printf("***triggered temp****\n");
+      sensor_data.type = PROCESS_QUEUE;
+      printf("size of message_t %d\n", sizeof(message_t));
+     if((nbytes = mq_send(temp_mq, (char*)&sensor_data, sizeof(message_t), 30)) == ERROR)
+    // if((nbytes = mq_send(temp_mq, proc_msg, 13, 30)) == ERROR)
+      {
+        perror("mq_send");
+      }
+      else
+      {
+        printf("1_T. sequencer to temp q: %d bytes: message successfully sent\n", nbytes);
+      }
+      break;
+
+    case 2:
+    printf("***triggered light****\n");
+    sensor_data.type = PROCESS_QUEUE;
+      if((nbytes = mq_send(light_mq, (char *)&sensor_data, sizeof(message_t), 30)) == ERROR)
+      // if((nbytes = mq_send(light_mq, proc_msg, 13, 30)) == ERROR)
+      {
+        perror("mq_send");
+      }
+      else
+      {
+        printf("1_L. sequencer to light q: message successfully sent\n");
+      }
+      count = 0;
+      break;
+    default:
+      break;
+  }
+}
+
+void *sequencerThread(void *threadp){
+
+
+  // create 2 timers (http://devlib.symbian.slions.net/s3/GUID-B4039418-6499-555D-AC24-9B49161299F2.html)
+  // 2 signals and 2 signal handlers
+
+  printf("\n sequencer thread \n");
+
+  int Ret;
+  // printf("\n in timer \n");
+
+  pthread_attr_t attr;
+  pthread_attr_init( &attr );
+
+  struct sched_param parm;
+  parm.sched_priority = 50;
+  pthread_attr_setschedparam(&attr, &parm);
+
+  struct sigevent sig;
+  sig.sigev_notify = SIGEV_THREAD;
+  sig.sigev_notify_function = sighler;
+  sig.sigev_value.sival_int =20;
+  sig.sigev_notify_attributes = &attr;
+
+  //create a new timer.
+  timer_t timerid;
+  Ret = timer_create(CLOCK_REALTIME, &sig, &timerid);
+  if (Ret == 0)
+      {
+      struct itimerspec in, out;
+      in.it_value.tv_sec = 4;
+      in.it_value.tv_nsec = 0;
+      in.it_interval.tv_sec = 0;
+      in.it_interval.tv_nsec = 100000000;
+      //issue the periodic timer request here.
+      Ret = timer_settime(timerid, 0, &in, &out);
+      // if(Ret == 0)
+      //     sleep(8);
+      // else
+      //     printf("timer_settime() failed with %d\n", errno);
+      //delete the timer.
+      while(1){
+        // printf("waiting on sensor_finish_sem sem\n");
+        // sem_wait(&sensor_finish_sem);
+        // printf("sequencer thread\n");
+        // sem_post(&procSem);
+        // sem_post(&logSem);
+      }
+      timer_delete(timerid);
+      }
+  else
+  printf("timer_create() failed with %d\n", errno);
+
+
+
+}
---
+++ src/temperature_driver.c
@@ -0,0 +1,52 @@
+#include "system.h"
+#include "sequencer.h"
+#include <sys/time.h>
+#include "TMP102.h"
+
+void *temperatureThread(void *threadp)
+{
+
+  int nbytes,prio;
+  message_t sensor_recv;
+  struct timeval tv;
+  int file;
+  char *filename = "/dev/i2c-2";
+  uint16_t data = 0;
+  i2c_init(&file,filename,TMP102_ADDR);
+ while(1){
+
+    printf("Waiting for temp_mq\n");
+    if((nbytes = mq_receive(temp_mq, (char *)&sensor_recv, MAX_MSG_SIZE, &prio)) == ERROR)
+    {
+      perror("mq_receive");
+    }
+    else
+    {
+
+      printf("temp receive: msg %d received with priority = %d, length = %d\n",
+           sensor_recv.type, prio, nbytes);
+      if(sensor_recv.type == PROCESS_QUEUE){
+        read_two_byte(&file,TEMPERATURE_REG,&data);
+        gettimeofday(&tv,NULL);
+        sensor_recv.sensor = TEMPERATURE;
+        sensor_recv.timestamp = tv.tv_sec;
+        sensor_recv.status = GOOD;
+        sensor_recv.data.temperatureData = ((uint8_t)data<<4| data>>12)*0.0625;
+
+        if((nbytes = mq_send(proc_mq, (char *)&sensor_recv, sizeof(sensor_recv), 30)) == ERROR)
+       // if((nbytes = mq_send(temp_mq, proc_msg, 13, 30)) == ERROR)
+         {
+           perror("mq_send");
+         }
+         else
+         {
+           printf("2_T. temp thread sending proc q: %d bytes: message successfully sent\n", nbytes);
+         }
+
+      }
+
+    }
+
+  }
+
+}
