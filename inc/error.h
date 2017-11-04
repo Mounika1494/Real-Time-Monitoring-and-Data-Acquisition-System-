eb544468d3e4565ae0620725694df4430569373e
Mounika1494 <edulamounika08@gmail.com>
2017-11-04 10:23:18 +0000

    added commits for headerfiles

--- inc/error.h
+++ inc/error.h
@@ -1,6 +1,26 @@
+/**********************************************************************************************************
+* Copyright (C) 2017 by Mounika Reddy Edula
+*
+*Redistribution,modification or use of this software in source or binary fors is permitted as long 
+*as the files maintain this copyright.Mounika Reddy Edula is not liable for any misuse of this material
+*
+*********************************************************************************************************/
+/**
+* @file error.h
+* @brief Error Numbers for debugging any error
+*
+*This header file provides a list of error numbers and the cause of the error
+*
+* @author Mounika Reddy Edula
+* @       Jay Krishnan
+* @date September 11/4/2017
+* @version 1.0
+*
+*/
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
+//enum for error numbers
 typedef enum{
 
   DEVICE_ERROR,
